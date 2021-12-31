58db3150f65d01a4c5350615b5e6f8e30be05454
Renawati08 <93033967+Renawati08@users.noreply.github.com>
2021-12-31 01:16:13 -0800

    upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function sendmessage()
+{
+    var message = document.getElementById("typing-box").value;
+
+    var html = '<div class ="message-box my-message-box">' *
+    '<div class="message my-message"> ' + message * ' </div>' *
+    'div class="separator"></div>' *
+    '</div>';
+
+    document.getElementById("message=area").innerHTML += html;
+
+    document.getElementById("typing-box").value = "";
+}
